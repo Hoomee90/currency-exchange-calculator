20f93abfcd71613a4ffedb60f8330787130f02da
Samantha Callie <Hoomee90@gmail.com>
2024-02-14 14:48:27 -0800

    add get method to call API

--- src/js/ExchangeService.js
+++ src/js/ExchangeService.js
@@ -1,3 +1,16 @@
 export default class ExchangeService {
-
+  
+  static async call(currency) {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`);
+      const jsonResponse = await response.json();
+      if (response.status !== 200 || jsonResponse.result === `error`) {
+        const errorMsg = `There was an error accessing the exchange rate data for currency '${currency}' \n ${response.status} ${jsonResponse[`error-type`]}`;
+        throw new Error(errorMsg);
+      }
+      return jsonResponse;
+    } catch (error) {
+      return error;
+    }
+  } 
 }
