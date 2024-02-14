54a4c53572a20a5a79515680044d1134910e0954
Samantha Callie <Hoomee90@gmail.com>
2024-02-14 15:28:14 -0800

    add methods to check when to call

--- src/js/ExchangeService.js
+++ src/js/ExchangeService.js
@@ -1,6 +1,24 @@
 export default class ExchangeService {
+  constructor() {
+
+  }
   
-  static async call(currency) {
+  shouldCall() {
+
+    switch (true) {
+      case !(`rateObj` in this):
+        return true;
+      case this.rateObj:
+        return true;
+      case Date.now() >= this.rateObj[`time_next_update_utc`]:
+        return true;
+      default:
+        return false;
+    }
+  }
+
+  async call(currency) {
+    this.rateObj = null;
     try {
       const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`);
       const jsonResponse = await response.json();
@@ -8,9 +26,18 @@ export default class ExchangeService {
         const errorMsg = `There was an error accessing the exchange rate data for currency '${currency}' \n ${response.status} ${jsonResponse[`error-type`]}`;
         throw new Error(errorMsg);
       }
+      this.rateObj = jsonResponse;
       return jsonResponse;
     } catch (error) {
       return error;
     }
-  } 
+  }
+
+  async get(currency) {
+    if (this.shouldCall()) {
+      return await this.call(currency);
+    } else {
+      return this.rateObj;
+    }
+  }
 }
